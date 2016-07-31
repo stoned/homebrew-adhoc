f9112e80663878c759c1d726fe84991cd2098b4b
Stoned Elipot <stoned.elipot@gmail.com>
2016-07-31 16:11:25 +0200

    update pkgsrc-bootstrap to version 2016Q2

--- pkgsrc-bootstrap.rb
+++ pkgsrc-bootstrap.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class PkgsrcBootstrap < Formula
   homepage 'http://www.pkgsrc.org'
-  url 'http://ftp.netbsd.org/pub/pkgsrc/stable/pkgsrc-2015Q3.tar.bz2'
-  version '2015Q3'
-  sha1 'e4715dff314afd4b325b783857a2d7c82bb46263'
+  url 'http://ftp.netbsd.org/pub/pkgsrc/stable/pkgsrc-2016Q2.tar.bz2'
+  version '2016Q2'
+  sha256 '2eb6ace0e083abc4c6202722bdf8e9add9bb1b9f76b6f845da390ca8fc15e7a7'
 
   def install
     ENV.deparallelize
